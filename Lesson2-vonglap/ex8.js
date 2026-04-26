0b9213701d472bfb49c256f906f90c0f0d5932dc
Sand Tran <hienphuong144@gmail.com>
2026-04-26 11:50:47 +0700

    lam them bai tap lesson 2

---
+++ Lesson2-vonglap/ex8.js
@@ -0,0 +1,3 @@
+for(let i = 1; i <= 100; i+=4){
+    console.log(`so cach nhau 4 so la: ${i} \n`);
+}
---
+++ Lesson2-vonglap/ex9.js
@@ -0,0 +1,3 @@
+for(let i = 25 ; i >= 12; i--){
+    console.log(`dem nguoc tu 25 ve 12 la : ${i} \n`)
+}
